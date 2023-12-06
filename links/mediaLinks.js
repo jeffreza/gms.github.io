6a95a7e81050dfd1797b836abf9e49b840ffbf92
Ali Hassan <hasanzaidi2896@gmail.com>
2023-12-06 15:10:33 +0500

    added media links

---
+++ links/mediaLinks.js
@@ -0,0 +1,5 @@
+var linkedin =
+  "https://www.linkedin.com/company/gmspakistan/posts/?feedView=all";
+var facebook = "https://www.facebook.com/GMS.WorldOfficial";
+var twitter = "https://twitter.com/gmspakistan";
+var googleplus = "https://plus.google.com/u/0/104671861391059967734";
